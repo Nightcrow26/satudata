e1d1b55830cfa9530a135a3fbfcdd5f075e111ec
Afrizal2609 <afrizal.bbm123@gmail.com>
2025-10-06 16:53:44 +0800

    Fix Model in SSO

--- app/Http/Controllers/SsoController.php
+++ app/Http/Controllers/SsoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
-use App\models\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Http;
 
